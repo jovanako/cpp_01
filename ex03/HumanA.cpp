8a78bf73aea293a8e4cef64e281185fd4f888b8d
Jovana <jovana1005@gmail.com>
2026-01-22 21:13:17 +0100

    added HumanA class

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,7 +6,21 @@
 /*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 11:17:03 by jkovacev          #+#    #+#             */
-/*   Updated: 2026/01/21 11:17:04 by jkovacev         ###   ########.fr       */
+/*   Updated: 2026/01/22 21:08:40 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+#include <iostream>
+
+HumanA::HumanA(const std::string name, Weapon& weapon)
+	: name(name), weapon(weapon) {}
+
+HumanA::~HumanA() {}
+
+void HumanA::attack() const {
+	std::cout << name
+			<< " attacks with their "
+			<< weapon.getType()
+			<< std::endl;
+}
--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,7 +6,27 @@
 /*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 11:16:54 by jkovacev          #+#    #+#             */
-/*   Updated: 2026/01/21 11:16:55 by jkovacev         ###   ########.fr       */
+/*   Updated: 2026/01/22 21:06:58 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include "Weapon.hpp"
+#include <string>
+
+class HumanA
+{
+	private:
+		std::string name;
+		Weapon& weapon;
+	
+	public:
+		HumanA(const std::string name, Weapon& weapon);
+		
+		~HumanA();
+
+		void attack() const;
+};
+#endif
